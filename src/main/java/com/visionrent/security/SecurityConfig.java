94b7bba0ace8514628c4e8750cc5bf8480776ba1
filibeli9 <filibelibalaban@gmail.com>
2023-01-27 20:29:24 +0000

    Swagger settings has been done

--- src/main/java/com/visionrent/security/SecurityConfig.java
+++ src/main/java/com/visionrent/security/SecurityConfig.java
@@ -3,17 +3,22 @@ package com.visionrent.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.visionrent.security.jwt.AuthTokenFilter;
 
@@ -30,18 +35,16 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable().
 			 sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
-			 and().
+			 and().// those two lines for delete related cors process
+			 authorizeRequests().antMatchers(HttpMethod.OPTIONS,"/**").permitAll().and().
 			 authorizeRequests().
-			 antMatchers("/", 
-						 "index.html", 
-						 "/login", 
+			 antMatchers("/login", 
 						 "/register", 
-						 "/js/*", 
-						 "/css/*",
-						 "/images/*",
+						 "/contactmessage/visitors",
 						 "/files/download/**",
 						 "/files/display/**",
-						 "/car/visitors/**").permitAll().
+						 "/car/visitors/**",
+						 "/actuator/info","/actuator/health").permitAll().
 				anyRequest().authenticated();
 
 		http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
@@ -49,6 +52,44 @@ public class SecurityConfig {
 		return http.build();
 	}
 
+	//*******************cors settings***************
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").
+										   allowedHeaders("*").
+										   allowedMethods("*");
+			}
+		};
+	}
+	//*******************SWAGGER*****************************
+	//http://localhost:8080/swagger-ui/index.html#/
+	private static final String[] AUTH_WHITE_LIST = {
+			"/v3/api-docs/**",
+			"/swagger-ui.html",
+			"/swagger-ui/**",
+			"/",
+			"/index.html",
+			"/images/**",
+			"/css/**",
+			"/js/**"
+	};
+	
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		WebSecurityCustomizer customizer = new WebSecurityCustomizer() {
+			
+			@Override
+			public void customize(WebSecurity web) {
+				web.ignoring().antMatchers(AUTH_WHITE_LIST);
+			}
+		};
+		return customizer;
+	}
+	
+	
 	@Bean
 	public AuthTokenFilter authTokenFilter() {
 		return new AuthTokenFilter();
